e2d6d0009168557149b61dc328c030bc6a85b666
litezevin <litezevin@gmail.com>
2025-11-11 03:40:35 +0600

    Disabled moonshotai/Kimi‑K2‑Thinking model support due to deprecation

--- src/vs/workbench/contrib/void/common/modelCapabilities.ts
+++ src/vs/workbench/contrib/void/common/modelCapabilities.ts
@@ -77,7 +77,7 @@ export const defaultProviderSettings = {
 export const defaultModelsOfProvider = {
 	edlide: [
 		'zai-org/GLM-4.6:THINKING',
-		'moonshotai/Kimi-K2-Thinking',
+		// 'moonshotai/Kimi-K2-Thinking', //
 		'MiniMaxAI/MiniMax-M2:THINKING',
 		'deepseek-ai/DeepSeek-V3.1-Terminus',
 		'openai/gpt-oss-20b' // Hidden SCM-only model for commit generation
