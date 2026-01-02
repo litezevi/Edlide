13480b398ed21722ae6a476a1bf37f28995dc8a0
litezevin <litezevin@gmail.com>
2026-01-02 12:57:22 +0600

    updated github actions

--- build/lib/electron.ts
+++ build/lib/electron.ts
@@ -196,7 +196,7 @@ export const config = {
 	darwinForceDarkModeSupport: true,
 	darwinCredits: darwinCreditsTemplate ? Buffer.from(darwinCreditsTemplate({ commit: commit, date: new Date().toISOString() })) : undefined,
 	linuxExecutableName: product.applicationName,
-	winIcon: 'resources/win32/code.ico',
+	winIcon: 'resources/win32/edlide.ico',
 	token: process.env['GITHUB_TOKEN'],
 	repo: product.electronRepository || undefined,
 	validateChecksum: true,
