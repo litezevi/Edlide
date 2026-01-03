018f885f065da9f7635a888c53fec2e4c493900a
litezevin <litezevin@gmail.com>
2026-01-03 14:36:37 +0600

    updated icons on windows(testing)

--- build/lib/electron.js
+++ build/lib/electron.js
@@ -215,7 +215,7 @@ exports.config = {
     darwinForceDarkModeSupport: true,
     darwinCredits: darwinCreditsTemplate ? Buffer.from(darwinCreditsTemplate({ commit: commit, date: new Date().toISOString() })) : undefined,
     linuxExecutableName: product.applicationName,
-    winIcon: 'resources/win32/code.ico',
+    winIcon: 'resources/win32/edlide.ico',
     token: process.env['GITHUB_TOKEN'],
     repo: product.electronRepository || undefined,
     validateChecksum: true,
